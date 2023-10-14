f80f2a33948aea3ce3f657e18dee9ecacf368fa5
Aaron T <info@aarontan.co.nz>
2023-10-15 01:57:16 +1300

    WIP - Angular environment setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,3 +16,41 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+// import { NgModule } from '@angular/core';
+// import { BrowserModule } from '@angular/platform-browser';
+// import { FormsModule } from '@angular/forms';
+// import { HttpClientModule } from '@angular/common/http';
+// import { GraphQLModule } from './graphql.module'; // import the GraphQLModule
+// import { AppState } from './state/app.state'; 
+// import { NgxsModule } from '@ngxs/store';
+// import { GraphqlService } from '@app/services/graphql.service';
+
+// import { AppComponent } from './app.component';
+// import { ContactsComponent } from './contacts/contacts.component';
+// import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
+// import { HeaderComponent } from './header/header.component';
+
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     ContactsComponent,
+//     ContactDetailsComponent,
+//     HeaderComponent
+//   ],
+//   imports: [
+//     BrowserModule,
+//     FormsModule,
+//     HttpClientModule,
+//     GraphQLModule,
+//     NgxsModule.forRoot([AppState]),
+//   ],
+//   providers: [
+//     GraphqlService,
+//   ],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
