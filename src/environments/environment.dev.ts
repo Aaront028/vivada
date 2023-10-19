1ef019ff18c41bd14ba71ff30fa990fe4fd5e47d
Aaron T <info@aarontan.co.nz>
2023-10-19 23:37:11 +1300

    update file

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'API_URL',
-  hasuraAdminSecret: 'HASURA_ADMIN_SECRET',
-  // Add other variables as needed
+  apiUrl: '${API_URL}',
+  hasuraAdminSecret: '${HASURA_ADMIN_SECRET}',
+  // ... other variables
 };
