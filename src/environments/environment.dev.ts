c4a980d09d263068ba764e38b1ad10232924e5a5
Aaron T <info@aarontan.co.nz>
2023-10-19 17:45:24 +1300

    WIP - Reset variable name

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'API_URL',
-  hasuraAdminSecret: 'HASURA_ADMIN_SECRET',
+  apiUrl: process.env["API_URL"] || 'default_api_url',
+  hasuraAdminSecret: process.env["HASURA_ADMIN_SECRET"] || 'default_hasura_admin_secret',
   // Add other variables as needed
 };
