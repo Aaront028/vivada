1387ef39fb54a0f24f40e0e99e55fc31b1b4e715
Aaron T <info@aarontan.co.nz>
2023-10-20 13:14:25 +1300

    change file in environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['API_URL'] || 'YOUR_DEFAULT_API_URL',
-  hasuraAdminSecret: process.env['API_KEY'] || 'YOUR_DEFAULT_API_KEY',
+  apiUrl:'YOUR_DEFAULT_API_URL',
+  hasuraAdminSecret: 'YOUR_DEFAULT_API_KEY',
   realm: 'vivada-ui-online',
 };
