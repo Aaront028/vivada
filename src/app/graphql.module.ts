92ee6020e94f1122cdf8d3049d0f137e190e6232
Aaron T <info@aarontan.co.nz>
2023-11-10 19:05:46 +1300

    WIP - Test npm install force fix

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,6 @@ import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { ApolloClientOptions, InMemoryCache, split } from '@apollo/client/core';
 import { WebSocketLink } from '@apollo/client/link/ws';
-import { getMainDefinition } from '@apollo/client/utilities';
 import { environment } from '../environments/environment';
 
 
@@ -12,34 +11,8 @@ const uri = environment.apiUrl;
 environment.testMessage && console.log(environment.testMessage);
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
-
-  const http = httpLink.create({
-    uri,
-  });
-
-  // WebSocket Link
-  const ws = new WebSocketLink({
-    uri: uri ? uri.replace("http", "ws") : '', // Adjust the WebSocket URL accordingly
-    options: {
-      reconnect: true,
-      connectionParams: {
-
-      },
-    },
-  });
-
-
-  const link = split(
-    ({ query }) => {
-      const definition = getMainDefinition(query);
-      return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
-    },
-    ws,
-    http,
-  );
-
   return {
-    link,
+    link: httpLink.create({ uri }),
     cache: new InMemoryCache(),
   };
 }
