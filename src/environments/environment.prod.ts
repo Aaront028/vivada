e8dace694cd56cf1ee6989cf3a8307a3ee0b74a9
Aaron T <info@aarontan.co.nz>
2023-11-10 19:13:38 +1300

    WIP - Add testMessage to production environment.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   production: true,
   apiUrl: process.env['NG_APP_API_URL'],
   realm: 'vivada',
-  clientId: 'vivada-online'
+  clientId: 'vivada-online',
+  testMessage: process.env['NG_APP_TEST_MESSAGE'] || 'You will only see this message in production and process.env.NG_APP_TEST_MESSAGE is not set'
+
 };
