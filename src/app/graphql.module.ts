39e983e9eb98170d50e85df1ead6ed1eaf7b33a2
Aaron T <info@aarontan.co.nz>
2023-11-10 19:28:19 +1300

    WIP - Remove comments and added split and websocket

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,16 +3,39 @@ import { APOLLO_OPTIONS, ApolloModule } from 'apollo-angular';
 import { HttpLink } from 'apollo-angular/http';
 import { ApolloClientOptions, InMemoryCache, split } from '@apollo/client/core';
 import { WebSocketLink } from '@apollo/client/link/ws';
+import { getMainDefinition } from '@apollo/client/utilities';
 import { environment } from '../environments/environment';
 
 
 const uri = environment.apiUrl;
-
 environment.testMessage && console.log(environment.testMessage);
 
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
+
+  const http = httpLink.create({
+    uri,
+
+  });
+
+  const ws = new WebSocketLink({
+    uri: uri ? uri.replace("http", "ws") : '',
+    options: {
+      reconnect: true,
+    },
+  });
+
+
+  const link = split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+    },
+    ws,
+    http,
+  );
+
   return {
-    link: httpLink.create({ uri }),
+    link,
     cache: new InMemoryCache(),
   };
 }
